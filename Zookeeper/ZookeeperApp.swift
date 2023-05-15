2ac16d6aa4f3139d06479c0f03fd3945a5aa7c9e
JaseTheJap <ammamusicschool@gmail.com>
2023-05-15 11:01:58 +0200

    Initial Commit

---
+++ Zookeeper/ZookeeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZookeeperApp.swift
+//  Zookeeper
+//
+//  Created by Jason Hattingh on 2023/05/15.
+//
+
+import SwiftUI
+
+@main
+struct ZookeeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

03a8610961dd64e800faae2fb54fc50efbb6eae7
JaseTheJap <ammamusicschool@gmail.com>
2023-05-17 09:42:46 +0200

    Initial Code For Zookeeper

--- Zookeeper/ContentView.swift
+++ Zookeeper/ContentView.swift
@@ -1,23 +1,61 @@
 //
 //  ContentView.swift
-//  Zookeeper
+//  Shared
 //
-//  Created by Jason Hattingh on 2023/05/15.
+//  Created by Henry Minden on 7/28/21.
 //
 
 import SwiftUI
 
 struct ContentView: View {
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        List {
+            ForEach(animalGroups) { animalGroup in
+                Section(header: Text(animalGroup.groupName)) {
+                    ForEach(animalGroup.animals) { animal in
+                        Text(animal.name)
+                    }
+                }
+            }
         }
-        .padding()
     }
+
 }
+
+struct Animal: Identifiable {
+    var id = UUID()
+    var name : String
+}
+
+struct AnimalGroup: Identifiable {
+    var id = UUID()
+    var groupName : String
+    var animals : [Animal]
+}
+
+let animalGroups = [
+    AnimalGroup(groupName: "Pets", animals: [
+        Animal(name: "Dog"),
+        Animal(name: "Cat"),
+        Animal(name: "Parrot"),
+        Animal(name: "Mouse")
+    ]),
+    AnimalGroup(groupName: "Farm", animals: [
+        Animal(name: "Cow"),
+        Animal(name: "Horse"),
+        Animal(name: "Goat"),
+        Animal(name: "Sheep"),
+    ]),
+    AnimalGroup(groupName: "Critters", animals: [
+        Animal(name: "Firefly"),
+        Animal(name: "Spider"),
+        Animal(name: "Ant"),
+        Animal(name: "Squirrel"),
+    ])
+]
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
